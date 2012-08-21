32c72a4dcc4741156559b4912d7ac5a1f540aeba
Oliver Rickard <ocrickard@MathBook Pro.(none)>
2012-08-21 10:25:02 -0700

    Initial Commit

---
+++ popover/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  popover
+//
+//  Created by Oliver Rickard on 21/08/2012.
+//  Copyright (c) 2012 Oliver Rickard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ popoverTests/popoverTests.m
@@ -0,0 +1,32 @@
+//
+//  popoverTests.m
+//  popoverTests
+//
+//  Created by Oliver Rickard on 21/08/2012.
+//  Copyright (c) 2012 Oliver Rickard. All rights reserved.
+//
+
+#import "popoverTests.h"
+
+@implementation popoverTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in popoverTests");
+}
+
+@end
