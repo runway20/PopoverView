50d741741faedd29790b76385ba621e0695ce700
Jindrich Dolezy <jindrich@dolezy.cz>
2014-03-13 13:43:52 +0100

    Nicer demo

--- popover/Demo/ViewController.m
+++ popover/Demo/ViewController.m
@@ -6,21 +6,42 @@
 //  Copyright (c) 2012 Oliver Rickard. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h> //This is just for the daysView where I call "daysView.layer" not necessary normally.
 #import "ViewController.h"
 #import "PopoverView.h"
-#import "PopoverView+ShowMethods.h"
 #import "PopoverView+StaticShowMethods.h"
 #import "PopoverView+ShowImage.h"
 #import "OCDaysView.h"
-#import <QuartzCore/QuartzCore.h> //This is just for the daysView where I call "daysView.layer" not necessary normally.
 
-#define kStringArray @[@"YES", @"NO"]
-#define kImageArray @[[UIImage imageNamed:@"success"], [UIImage imageNamed:@"error"]]
 
-@interface ViewController () <PopoverViewDelegate, UITableViewDataSource, UITableViewDelegate>
+#define kStringArray @[ \
+    @"YES", \
+    @"NO"]
 
-@property (nonatomic, weak) IBOutlet UILabel *tapAnywhereLabel;
-@property (nonatomic, strong) PopoverView *popoverView;
+#define kImageArray @[ \
+    [UIImage imageNamed:@"success"], \
+    [UIImage imageNamed:@"error"]]
+
+// this must be in sync with createPopoverAtPoint: switch
+#define kDemoOptions @[ \
+    @"Long text", \
+    @"Text with title", \
+    @"String array", \
+    @"String array with title", \
+    @"String & image array", \
+    @"String & image array with title", \
+    @"Custom view", \
+    @"Custom view with title", \
+    @"Table view with title"]
+
+
+@interface ViewController () <PopoverViewDelegate, UITableViewDataSource, UITableViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate>
+
+@property(nonatomic, weak) IBOutlet UILabel *tapAnywhereLabel;
+@property(nonatomic, weak) IBOutlet UIPickerView *pickerView;
+@property(nonatomic, weak) IBOutlet UISwitch *dismissSwitch;
+
+@property(nonatomic, strong) PopoverView *popoverView;
 
 @end
 
@@ -29,96 +50,82 @@
 
 #pragma mark EXAMPLE CODE IS HERE
 
-- (IBAction)tapped:(UITapGestureRecognizer *)tap {
-    CGPoint point = [tap locationInView:self.view];
-    //NSLog(@"tapped at %@", NSStringFromCGPoint(point));
-
+- (PopoverView *)createPopoverAtPoint:(CGPoint)point {
     // Here are a couple of different options for how to display the Popover
 
     // PopoverView can be styled using appearance
     //[[PopoverView appearance] setGradientBottomColor:[UIColor redColor]];
 
+    switch ([self.pickerView selectedRowInComponent:0]) {
+        default:
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withText:@"This is a very long popover box.  As you can see, it goes to multiple lines in size."
+                    delegate:self]; // Show text wrapping popover with long string
 
-//    pv = [PopoverView showPopoverAtPoint:point
-//                                  inView:self.view
-//                                withText:@"This is a very long popover box.  As you can see, it goes to multiple lines in size." delegate:self]; // Show text wrapping popover with long string
+        case 1:
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withTitle:@"This is a title"
+                    withText:@"This is text"
+                    delegate:self]; // Show text with title
 
-//    pv = [PopoverView showPopoverAtPoint:point
-//                                  inView:self.view
-//                               withTitle:@"This is a title"
-//                                withText:@"This is text"
-//                                delegate:self]; // Show text with title
 
-//    pv = [PopoverView showPopoverAtPoint:point
-//                                  inView:self.view
-//                         withStringArray:kStringArray
-//                                delegate:self]; // Show the string array defined at top of this file
+        case 2:
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withStringArray:kStringArray
+                    delegate:self]; // Show the string array defined at top of this file
+        case 3:
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withTitle:@"Was this helpful?"
+                    withStringArray:kStringArray
+                    delegate:self]; // Show string array defined at top of this file with title.
+        case 4:
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withStringArray:kStringArray
+                    withImageArray:kImageArray
+                    delegate:self];
 
-    self.popoverView = [PopoverView showPopoverAtPoint:point
-            inView:self.view
-            withTitle:@"Was this helpful?"
-            withStringArray:kStringArray
-            delegate:self]; // Show string array defined at top of this file with title.
+        case 5:
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withTitle:@"This is a title"
+                    withStringArray:kStringArray
+                    withImageArray:kImageArray
+                    delegate:self];
 
-//    pv = [PopoverView showPopoverAtPoint:point
-//                                  inView:self.view
-//                         withStringArray:kStringArray
-//                          withImageArray:kImageArray
-//                                delegate:self];
+        case 6:
+            // Here's a little bit more advanced sample.  I create a custom view, and hand it off to
+            // the PopoverView to display for me.  I round the corners
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withContentView:[self createCustomView]
+                    delegate:self]; // Show calendar with no title
 
-//    pv = [PopoverView showPopoverAtPoint:point
-//                                  inView:self.view
-//                               withTitle:@"DEBUG"
-//                         withStringArray:kStringArray
-//                          withImageArray:kImageArray
-//                                delegate:self];
 
-//    // Here's a little bit more advanced sample.  I create a custom view, and hand it off to the PopoverView to display for me.  I round the corners
-//    OCDaysView *daysView = [[OCDaysView alloc] initWithFrame:CGRectMake(0, 0, 150, 100)];
-//    [daysView setMonth:10];
-//    [daysView setYear:2012];
-//    daysView.backgroundColor = [UIColor colorWithWhite:0.95f alpha:1.f]; //Give it a background color
-//    daysView.layer.borderColor = [UIColor colorWithWhite:0.9f alpha:1.f].CGColor; //Add a border
-//    daysView.layer.borderWidth = 0.5f; //One retina pixel width
-//    daysView.layer.cornerRadius = 4.f;
-//    daysView.layer.masksToBounds = YES;
-//    
-//    pv = [PopoverView showPopoverAtPoint:point
-//                                  inView:self.view
-//                         withContentView:[daysView autorelease]
-//                                delegate:self]; // Show calendar with no title
-//    pv = [PopoverView showPopoverAtPoint:point
-//                                  inView:self.view
-//                               withTitle:@"October 2012"
-//                         withContentView:[daysView autorelease]
-//                                delegate:self]; // Show calendar with title
+        case 7:
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withTitle:@"October 2012"
+                    withContentView:[self createCustomView]
+                    delegate:self]; // Show calendar with title
 
-//    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-//    tableView.delegate = self;
-//    tableView.dataSource = self;
-//    pv = [PopoverView showPopoverAtPoint:point
-//                                   inView:self.view
-//                          withContentView:tableView
-//                                 delegate:self];
+        case 8: {
+            UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+            tableView.delegate = self;
+            tableView.dataSource = self;
+            tableView.backgroundColor = [UIColor clearColor];
+            return [PopoverView showPopoverAtPoint:point
+                    inView:self.view
+                    withContentView:tableView
+                    delegate:self];
+        }
+    }
 }
 
-#pragma mark - DEMO - UITableView Delegate Methods
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 25;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
-    cell.textLabel.text = @"text";
-    cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:12.f];
-
-    return cell;
-}
 
 #pragma mark - PopoverViewDelegate Methods
 
@@ -145,6 +152,30 @@
     self.popoverView = nil;
 }
 
+
+#pragma mark - DEMO - UITableView Delegate Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 25;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    cell.textLabel.text = @"text";
+    cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:12.f];
+
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    cell.backgroundView.backgroundColor = cell.backgroundColor = tableView.backgroundColor;
+}
+
+
 #pragma mark - UIViewController Methods
 
 - (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
@@ -153,14 +184,44 @@
 
     if (self.popoverView) {
         // popover is visible, so we need to either reposition or dismiss it (dismising is probably best to avoid confusion)
-        BOOL dismiss = NO;
-        if (dismiss) {
+        if (self.dismissSwitch.on) {
             [self.popoverView dismiss:NO];
         } else {
-            // move popover
             [self.popoverView layoutAtPoint:center inView:self.view duration:duration];
         }
     }
+}
+
+
+#pragma mark - Demo view handling
+
+- (IBAction)tapped:(UITapGestureRecognizer *)tap {
+    CGPoint point = [tap locationInView:self.view];
+    self.popoverView = [self createPopoverAtPoint:point];
+}
+
+- (UIView *)createCustomView {
+    OCDaysView *daysView = [[OCDaysView alloc] initWithFrame:CGRectMake(0, 0, 150, 100)];
+    [daysView setMonth:10];
+    [daysView setYear:2012];
+    daysView.backgroundColor = [UIColor colorWithWhite:0.95f alpha:1.f]; //Give it a background color
+    daysView.layer.borderColor = [UIColor colorWithWhite:0.9f alpha:1.f].CGColor; //Add a border
+    daysView.layer.borderWidth = 0.5f; //One retina pixel width
+    daysView.layer.cornerRadius = 4.f;
+    daysView.layer.masksToBounds = YES;
+    return daysView;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return kDemoOptions[(NSUInteger) row];
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return kDemoOptions.count;
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
 }
 
 @end
